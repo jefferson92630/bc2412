949159280ec0e81c10644bcbb1d61dcb7f43d7f6
jefferson.kwong <jefferson92630@yahoo.com.hk>
2024-12-10 12:43:23 +0800

    20241210

---
+++ demo-helloworld/src/Exercise20241210.java
@@ -0,0 +1,11 @@
+public class Exercise20241210 {
+  public static void main(String[] args) {
+    double priceOfApple = 7.3;
+    double priceOfOrange = 6.5;
+    int quantityOfApple = 3;
+    int quantityOfOrange = 4;
+
+    int totalamount = quantityOfApple + quantityOfOrange;
+    System.out.println(totalamount); // 7
+  }
+}
